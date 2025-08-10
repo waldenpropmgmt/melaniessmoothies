491d76e9c9c1b17fd27ed6ae19438da3f278e2ad
waldenpropmgmt <waldenpropmgmt@gmail.com>
2025-08-10 09:00:30 -0500

    Update streamlit_app.py
    add api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(name_on_order + ', your smoothie is ordered!', icon="👍")
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
